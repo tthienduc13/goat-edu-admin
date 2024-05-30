79c21ffe8fcdad8e4df1ede3be1b4bc507139a36
tthienduc13 <ducnltdev@gmail.com>
2024-05-30 12:56:51 +0700

    fix

--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -72,16 +72,6 @@ export default function UserAuthForm() {
           </Button>
         </form>
       </Form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
     </>
   );
 }
